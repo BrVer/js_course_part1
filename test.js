6f6b3c72230258867db3feeaeb143a9c5e4f6687
Dmitry Vlasov <d.vlasov.work@gmail.com>
2015-09-10 11:57:36 +0300

    task1 added

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 // assuming we have 'clear' arrays, without manually added enumerable properties
 var array = [6, 8, 10, 7, 2, 1, 9, 3, 5, 4];
+
+task1.test(array.slice());
 task2_1.test(array, function (x) {
     return x % 5 == 0;
 });
@@ -13,7 +15,8 @@ task4.test(array, function (x) {
     return x * x;
 });
 task5.test(array);
-task6_1.test(function(robot){
+
+test_robot_func = function(robot){
     robot.move();
     robot.move();
     robot.move();
@@ -21,13 +24,6 @@ task6_1.test(function(robot){
     robot.move();
     robot.move();
     robot.report(); // should be "2,3,EAST"
-});
-task6_2.test(function(robot){
-    robot.move();
-    robot.move();
-    robot.move();
-    robot.right();
-    robot.move();
-    robot.move();
-    robot.report(); // should be "2,3,EAST"
-});
+};
+task6_1.test(test_robot_func);
+task6_2.test(test_robot_func);
